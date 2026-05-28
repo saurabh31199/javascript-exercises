0cc1388e880ce8fad494f70ea8086ed9be86b6b6
loneWolf311 <saurabhprakashdwivedi@gmail.com>
2026-05-28 14:03:13 +0530

    Javascript Basics

---
+++ Assignment in JS/script.js
@@ -0,0 +1,20 @@
+//direct calculations using console.log()
+console.log(23+97);
+console.log(15+32+15+23+56+47);
+console.log((4+6+9)/77);
+
+//using variables
+let a = 10;
+console.log("Value of a: ", a);
+a=20;
+console.log("Updated value of a: ", a);
+
+//using a variable to compute another
+let b = 7*a;
+console.log("Value of b:", b);
+
+//using another sequence
+const max = 57;
+const actual = max-13;
+const percentage = actual/max;
+console.log("Value of percentage: ", percentage);
---
+++ First JS Program/javascript.js
@@ -0,0 +1,9 @@
+console.log("This script is added through external javascript code linked to a HTML.")
+
+//to see what an  updated value looks in console.log()
+let age=11;
+console.log(age);    //initial age value 11
+
+age=54;
+
+console.log(age);    //updated age value 54
